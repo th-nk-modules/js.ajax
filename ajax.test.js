2beda4e74777e72e64cc6e98bb54580abd978e15
sion evans <sionevans@gmail.com>
2015-08-12 08:50:26 +0100

    added test file

---
+++ ajax.test.js
@@ -0,0 +1,11 @@
+import {Ajax} from './ajax';
+
+describe('Ajax tests', function() {
+
+    it('Make sure things work', function() {
+        var result = 'ajax test';
+        expect(result).toBe('ajax test');
+    });
+ 
+});
+
